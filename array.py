bcbbfda9e35e651c3aa42c7c5d798338ed1112f3
evaldobarros <evaldobarros@gmail.com>
2023-09-13 22:20:59 -0300

    atualizaçao

---
+++ array.py
@@ -0,0 +1,8 @@
+dif achar_elemento(arr):
+  achou = False
+  cumprimento = len(arr)
+  for i in range(cumprimento)
+   print(arr [i])
+
+ordem = ['Primeiro','Segundo','Terceiro']
+achar_elemento(ordem)
